adf12ba190f2f0f0be2b959126c5e4f60697d40e
Troang <khanhtrang.280695@gmail.com>
2019-02-23 18:00:14 +0700

    Topic 1 no content

---
+++ src/base/Topic01.java
@@ -0,0 +1,20 @@
+package base;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Topic01 {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
